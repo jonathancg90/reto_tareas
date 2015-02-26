7632b499123fc20d28fc85a4edaa95fc08c06d36
jonathancg90 <jonathancg90@gmail.com>
2015-02-25 23:24:44 -0500

    create database

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from app import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+
+    def __repr__(self):
+        return '<Task %r>' % (self.name)
