962069e80aa643e8177cdd40a5365a3bc38e674e
jonathancg90 <jonathancg90@gmail.com>
2015-02-25 23:38:14 -0500

    structure: views

--- app/views.py
+++ app/views.py
@@ -5,3 +5,15 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/task/create')
+def task_create():
+    return render_template('index.html')
+
+@app.route('/task/delete/<pk>')
+def task_delete():
+    return render_template('index.html')
+
+@app.route('/task')
+def task_list():
+    return render_template('index.html')
