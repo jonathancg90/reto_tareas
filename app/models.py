8401d4cd17ff69987e62336e06ab624f93932373
jonathancg90 <jonathancg90@gmail.com>
2015-02-26 00:01:30 -0500

    change: model task

--- app/models.py
+++ app/models.py
@@ -2,6 +2,15 @@ from app import db
 
 
 class Task(db.Model):
+    STATUS_WAIT = 1
+    STATUS_EXECUTE = 2
+    STATUS_COMPLETE = 2
+    STATUS_CHOICES = (
+        (STATUS_WAIT, 'En espera'),
+        (STATUS_EXECUTE, 'Ejecutandose'),
+        (STATUS_COMPLETE, 'Ejecutado')
+    )
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(
         db.String(64),
@@ -10,6 +19,11 @@ class Task(db.Model):
     )
     time = db.Column(
         db.Integer,
+    )
+
+    status = db.Column(
+        db.SmallInteger,
+        default=STATUS_WAIT
     )
 
     def __repr__(self):
